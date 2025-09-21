47c76628466f2d7267aa195b11242f017175c0d8
Leandro-Kolosque <161510235+Leandro-Kolosque@users.noreply.github.com>
2025-09-21 20:13:20 -0300

    Update RecentProjects

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -30,9 +30,9 @@ const Hero = () => {
       <div className='flex justify-center relative my-20 z-10 '>
         
         <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
-          <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Dynamic Web Magic with Next.js</h2>
+          <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Dynamic Web Magic</h2>
           <TextGenerateEffect words="Transforming Concepts into Seamless User Experiences" className="text-center text-[40px] md:text-5xl lg:text-6xl"/>
-          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hi, I'm Leandro a Next.js Developer based in Brazil.</p>
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hi, I'm Leandro a Developer based in Brazil.</p>
           <a href="#about">
             <MagicButton
               title="Show my work"
