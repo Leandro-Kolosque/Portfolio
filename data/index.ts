ba3a56273d0450bed100e7d63517d7925d904422
Leandro-Kolosque <161510235+Leandro-Kolosque@users.noreply.github.com>
2025-09-23 14:43:35 -0300

    Update Footer links

--- data/index.ts
+++ data/index.ts
@@ -206,14 +206,12 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/Leandro-Kolosque",
   },
   {
     id: 2,
-    img: "/twit.svg",
-  },
-  {
-    id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/leandro-kolosque/",
   },
 ];
 
